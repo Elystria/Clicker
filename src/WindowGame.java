c56c31a76b0cbc4e0850586370adff089e428fc8
Elystria <petitevague@hotmail.fr>
2017-09-05 19:30:39 +0200

    Ajout d'un spritesheet test (ne bouge pas)

--- src/WindowGame.java
+++ src/WindowGame.java
@@ -3,9 +3,13 @@ import org.newdawn.slick.*;
 public class WindowGame extends BasicGame {
 
     private GameContainer container;
+    //Position départ perso
     private float x = 300, y = 300;
+    //Direction départ perso
     private int direction = 2;
-    private boolean moving = true;
+    //animation marche
+    private boolean moving = false;
+    // Tableau de srpites
     private Animation[] animations = new Animation[8];
 
     public WindowGame() {
@@ -18,10 +22,12 @@ public class WindowGame extends BasicGame {
 
         //Chargement et création de l'animation du perso
         SpriteSheet spriteSheet = new SpriteSheet("resources/sprites/character.png", 64, 64);
+        //Animation fixe 1 sprite
         this.animations[0] = loadAnimation(spriteSheet, 0, 1, 0);
         this.animations[1] = loadAnimation(spriteSheet, 0, 1, 1);
         this.animations[2] = loadAnimation(spriteSheet, 0, 1, 2);
         this.animations[3] = loadAnimation(spriteSheet, 0, 1, 3);
+        //Animations avec plusieurs sprite
         this.animations[4] = loadAnimation(spriteSheet, 1, 9, 0);
         this.animations[5] = loadAnimation(spriteSheet, 1, 9, 1);
         this.animations[6] = loadAnimation(spriteSheet, 1, 9, 2);
@@ -38,9 +44,23 @@ public class WindowGame extends BasicGame {
     }
 
     @Override
+    public void keyPressed(int key, char c) {
+        switch (key) {
+            //Mouvement du personnage
+            case Input.KEY_UP:    this.direction = 0; this.moving = true; break;
+            case Input.KEY_LEFT:  this.direction = 1; this.moving = true; break;
+            case Input.KEY_DOWN:  this.direction = 2; this.moving = true; break;
+            case Input.KEY_RIGHT: this.direction = 3; this.moving = true; break;
+        }
+    }
+
+    @Override
     public void keyReleased(int key, char c){
         if (Input.KEY_ESCAPE == key){
             container.exit();
+        }
+        if (Input.KEY_UP == key || Input.KEY_DOWN == key ||Input.KEY_LEFT == key || Input.KEY_RIGHT == key ){
+            this.moving = false;
         }
     }
 
