7e9a9f636e54d71ef2a173c66b6259b2b14ac20d
Dussier Alexys <adussier@localhost.localdomain>
2017-12-01 16:23:57 +0100

    ajout des fontions statiques de Bot

--- src/Bot.java
+++ src/Bot.java
@@ -32,31 +32,97 @@ public class Bot {
 		this.prixInitial = prixInitial;
 		this.rapportDEfficacite = rapportDEfficacite;
 		this.coefMultiplicite = coefMultiplicite;
+		this.nbPossede = 0;
 	}
 	
 	/***************************************************
 	METHODES
 	***************************************************/
 
-	// On va générer les getteurs particuliers
-	//- p(i+1) = 10 * (1 + 1/2*sin(2*pie*i/5)) * p(i)
-	//- r(i+1) = 1.6 * (1 + 1/4*cos(2*pie*i/5)) * r(i)
-	//- m(i+1) = (1.02)^i * m(i)
-
 	public int getPrixNextBot() {
-		int prix = 
+		return (int) (prixInitial * Math.pow(coefMultiplicite, nbPossede));
 	}
 	
 	public float getPPS() {
-		
+		return prixInitial * rapportDEfficacite;
 	}
 	
 	public float getPPSTotal() {
-		
+		return getPPS() * nbPossede;
+	}
+	
+	/***************************************************
+	METHODES STATIQUES
+	***************************************************/
+//	- p(i+1) = 10 * (1 + 1/2*sin(2*pie*i/5)) * p(i)
+//			- r(i+1) = 1.6 * (1 + 1/4*cos(2*pie*i/5)) * r(i)
+//			- m(i+1) = (1.02)^i * m(i)
+
+	public int prixInitialNextBot(Bot botPrecedant, int index) {
+		int pi = botPrecedant.getPrixInitial();
+		return (int) (10 * (1 + 1/2 * Math.sin(2 * Math.PI * index/5)) * pi);
+	}
+	
+	public float rapportDEfficaciteNextBot(Bot botPrecedant, int index) {
+		float ri = botPrecedant.getRapportDEfficacite();
+		return (float) (1.6 * (1 + 1/4 * Math.cos(2 * Math.PI * index/5)) * ri);
+	}
+	
+	public float coefMultipliciteNextBot(Bot botPrecedant, int index) {
+		float mi = botPrecedant.getCoefMultiplicite();
+		return (float) (Math.pow(1.02,  index) * mi);
 	}
 	
 	/***************************************************
 	GETTEURS && SETTEURS
 	***************************************************/
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public int getPrixInitial() {
+		return prixInitial;
+	}
+
+	public void setPrixInitial(int prixInitial) {
+		this.prixInitial = prixInitial;
+	}
+
+	public float getRapportDEfficacite() {
+		return rapportDEfficacite;
+	}
+
+	public void setRapportDEfficacite(float rapportDEfficacite) {
+		this.rapportDEfficacite = rapportDEfficacite;
+	}
+
+	public float getCoefMultiplicite() {
+		return coefMultiplicite;
+	}
+
+	public void setCoefMultiplicite(float coefMultiplicite) {
+		this.coefMultiplicite = coefMultiplicite;
+	}
+
+	public int getNbPossede() {
+		return nbPossede;
+	}
+
+	public void setNbPossede(int nbPossede) {
+		this.nbPossede = nbPossede;
+	}
+	
 }
